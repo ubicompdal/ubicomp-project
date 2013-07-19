8a5630956a9c1c32997418de5ed558e2b9ca058e
kharidia <sahilkharidia@yahoo.co.in>
2013-07-19 16:22:52 -0300

    Update clock.js
    Have added reset and restart button with start and stop functionality. Timer is on when screen is shut off. Find gps before start button is pressed.

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,93 @@
 var h=0, m=0, s=0, ms=0;
 var timer;
+
+var	clsStopwatch = function() {
+		// Private vars
+		var	startAt	= 0;	// Time of last start / resume. (0 if not running)
+		var	lapTime	= 0;	// Time on the clock when last stopped in milliseconds
+
+		var	now	= function() {
+				return (new Date()).getTime(); 
+			}; 
+ 
+		// Public methods
+		// Start or resume
+		this.start = function() {
+				startAt	= startAt ? startAt : now();
+			};
+
+		// Stop or pause
+		this.stop = function() {
+				// If running, update elapsed time otherwise keep it
+				lapTime	= startAt ? lapTime + now() - startAt : lapTime;
+				startAt	= 0; // Paused
+			};
+
+		// Reset
+		this.reset = function() {
+				lapTime = startAt = 0;
+			};
+
+		// Duration
+		this.time = function() {
+				return lapTime + (startAt ? now() - startAt : 0); 
+			};
+	};
+
+var x = new clsStopwatch();
+var $time;
+var clocktimer;
+
+function pad(num, size) {
+	var s = "0000" + num;
+	return s.substr(s.length - size);
+}
+
+function formatTime(time) {
+	var h = m = s = ms = 0;
+	var newTime = '';
+
+	h = Math.floor( time / (60 * 60 * 1000) );
+	time = time % (60 * 60 * 1000);
+	m = Math.floor( time / (60 * 1000) );
+	time = time % (60 * 1000);
+	s = Math.floor( time / 1000 );
+	ms = time % 1000;
+
+	newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+	return newTime;
+}
+
+function show() {
+	
+	$time = document.getElementById('time');
+	
+	update();
+}
+
+function update() {
+	
+	$time.innerHTML ="<i class=icon-time></i>" + " "+formatTime(x.time());
+}
+
+function start() {
+	clocktimer = setInterval("update()", 1);
+	x.start();
+}
+
+function stop() {
+	x.stop();
+	clearInterval(clocktimer);
+}
+
+function reset() {
+	stop();
+	x.reset();
+	update();
+}
+
+
+
 function showTime()
 {
 	var today=new Date();
@@ -15,10 +103,12 @@ function showTime()
 		m = 0;
 		h++;
 	}
-	$("#clock .value").text(checkTime(h)+":"+checkTime(m)+":"+checkTime(s)+"."+checkTime(ms));
+	var temp= $("#time").text(checkTime(h)+":"+checkTime(m)+":"+checkTime(s)+"."+checkTime(ms));
 	ms++;
-	timer=setTimeout('showTime()', 10);
-}
+	timer=setInterval('showTime()', 10);
+	
+	}
+
 // add a zero in front of numbers<10
 function checkTime(i)
 {
@@ -28,18 +118,43 @@ function checkTime(i)
 	}
 	return i;    
 }
+var count=0;
 
 $(document).ready(function(){
+document.getElementById("start").value="Start";
+
+$("#reset").hide();
 	$("#start").click(function(){
-		showTime();
+	
+clocktimer = setInterval("update()", 1);
+	x.start();
 		$(this).hide();
-		$("#stop").show();
+		 $("#stop").show(); 
+		 $("#reset").show();
+		 $("#stop").css("width","50%");
+			
 	});
 	$("#stop").click(function(){        
-        App.triggerEvent("stop:clock", getTimeInMilliseconds(h, m, s, ms));        
-		h=0; m=0; s=0;
-		clearTimeout(timer);
+	document.getElementById("start").value="Restart";
+        App.triggerEvent("stop:clock", getTimeInMilliseconds(h, m, s, ms));        		
+		stop();		
 		$(this).hide();
 		$("#start").show();
+		
+		$("#reset").show();
+		$("#start").css("width","50%");
+	
 	});
+	$("#reset").click(function(){
+	
+	document.getElementById("start").value="Start";
+	$("#start").show();
+	$(this).hide();
+	$("#stop").hide();
+	h=0;m=0;s=0;
+	reset();	
+	$("#time").text("00"+":"+"00"+":"+"00"+"."+"00");
+	$("#start").css("width","100%");
+	});
+	
 });
